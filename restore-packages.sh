d646479066c9e972be875291f461b8b6d7acba3e
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-28 15:11:28 -0300

    :sparkles: feat 03 class done

--- restore-packages.sh
+++ restore-packages.sh
@@ -1,7 +1,11 @@
+rm -rf **/**/node-modules
+cp -r aula02 aula03
+cd aula03
 for item in `ls`;
 do 
     echo $item
     cd $item
     npm ci --silent
+    yarn install --frozen-lockfile
     cd ..
 done
